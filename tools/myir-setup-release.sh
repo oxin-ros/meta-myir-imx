ed94fc4377990ffa715415cef003ef71fdb17afa
David-Michael Buckman <dmbuck32@gmail.com>
2023-04-19 16:41:20 +1200

    add wireguard tools.

--- tools/myir-setup-release.sh
+++ tools/myir-setup-release.sh
@@ -158,6 +158,9 @@ echo >> conf/local.conf
 echo "# Switch to Debian packaging and include package-management in the image" >> conf/local.conf
 echo "PACKAGE_CLASSES = \"package_deb\"" >> conf/local.conf
 echo "EXTRA_IMAGE_FEATURES += \"package-management\"" >> conf/local.conf
+echo "# Add WireGuard tools to the image" >> conf/local.conf
+echo "IMAGE_INSTALL += \"wireguard-tools\"" >> conf/local.conf
+
 
 if [ ! -e $BUILD_DIR/conf/bblayers.conf.org ]; then
     cp $BUILD_DIR/conf/bblayers.conf $BUILD_DIR/conf/bblayers.conf.org
